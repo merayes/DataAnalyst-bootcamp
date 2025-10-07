db32ee27b6399aff884f141c66befa85fba88445
merayes <120264154+merayes@users.noreply.github.com>
2025-10-07 14:26:45 +0300

    Add files via upload

---
+++ numpywork-2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # NORMAL DİSTRİBUTİON
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+np.random.normal(0,1,(4,5))
+
+
+# In[4]:
+
+
+np.random.normal(10,100,(4,5))
+
+
+# In[5]:
+
+
+np.random.normal(10,1,(4,5))
+
+
+# In[ ]:
+
+
+
+
